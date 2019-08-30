b05c0137481fb9d92eb73d2e23bb5b5bef3fcb51
jed <jed@m6700.lan>
2019-08-29 23:34:46 -0400

    .

---
+++ Blink/Blink.ino
@@ -0,0 +1,19 @@
+
+#pragma section FLASH_Parameter
+volatile const uint8_t pin = 13;
+
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(pin, OUTPUT);
+  Serial.begin(15200);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  Serial.print("Pin: ");
+  Serial.println(pin);
+  digitalWrite(pin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(pin, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+}
